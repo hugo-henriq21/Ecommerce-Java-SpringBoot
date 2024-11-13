cfa98e1257b547816d9a8a09f32aa66aa6dedb4c
labinfo4 <labinfo4@ectufrn.ect.ufrn.br>
2024-11-13 14:52:47 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/demo/layers/entities/Cliente.java
@@ -0,0 +1,51 @@
+package com.example.demo.layers.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 14)
+    private String cpf;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private Date dataNascimento;
+
+    //Getters e Setters
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+}
